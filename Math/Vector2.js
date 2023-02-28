76d6bd69435f0683631dabeac7262401fccc89ed
Brandon123b <87343219+Brandon123b@users.noreply.github.com>
2023-02-27 19:16:42 -0700

    Updated Vector2.js to rotate inplace and return a reference to itself instead of creating a new point every call

--- Math/Vector2.js
+++ Math/Vector2.js
@@ -6,23 +6,30 @@
 
 // rotate this vector to create a new one
 // provide outPoint argument to store the results, or leave it empty to return a new one
-PIXI.Point.prototype.rotate = function(_angle, outPoint) {
+PIXI.Point.prototype.rotate = function(_angle) {
+
+    // Calculate the sin and cos of the angle
     const cos = Math.cos(_angle * Math.PI / 180)
     const sin = Math.sin(_angle * Math.PI / 180)
 
-    // if no outPoint is given then make a new one to return
-    if (!outPoint) outPoint = new PIXI.Point()
-    outPoint.x = this.x * cos - this.y * sin
-    outPoint.y = this.x * sin + this.y * cos
-    return outPoint
-}
+    var oldX = this.x;
 
-// rotate a vector in place
-PIXI.Point.prototype.rotateInPlace = function(_angle) {
-    return this.rotate(_angle, this)
+    // Rotate the point
+    this.x = this.x * cos - this.y * sin
+    this.y = oldX   * sin + this.y * cos
+
+    // Return just in case we want to chain
+    return this
 }
 
 // get the angle of the vector in degrees
 PIXI.Point.prototype.getAngle = function() {
     return Math.atan2(this.y, this.x) * 180 / Math.PI
+}
+
+// Adds an x and y value to this vector
+PIXI.Point.prototype.addXY = function(_x, _y) {
+    this.x += _x
+    this.y += _y
+    return this
 }
