0646d0336d14ada06bbbe58b5554c254c1e8cb7f
NicolasWinsten <nicolasd.winsten@gmail.com>
2023-03-03 18:44:39 -0700

    make drag code generalizable to any action

--- drawing.js
+++ drawing.js
@@ -49,8 +49,7 @@ class Canvas {
   })
 
   // set up callbacks for mouse drag behavior (for panning)
-  mkDraggable(this.backGround, this.container)
-  
+  createDragAction(this.backGround, this.container)
 
   // on mouse wheel, change the zoom level
   let onScroll = e => {
@@ -105,22 +104,33 @@ class Canvas {
 
 // register events to make an object respond to mouse dragging
 // registerDisplayObject: display object to register the intial mouse drag
-// dragTarget: the displayObject whose coordinate space we use to calculate mouse position change
-// displaceFunc: function (dx, dy) => ... action to perform given mouse displacement (by default, simply translate dragTarget's position)
-function mkDraggable(registerDisplayObject, dragTarget, displaceFunc) {
+// dragTarget: the displayObject whose coordinate space we use to translate mouse coordinates into 
+//
+// these 3 parameters are procedures to perform during the mouse dragging
+// they each take the coordinates of the mouse translated into the dragTarget's parent's coordinate space
+// dragStartAction: function (x, y) => ... action to take when dragging begins
+// dragMoveFunction: function (dx, dy) => ... action to perform given mouse displacement (by default, simply translate dragTarget's position)
+// dragEndAction: function (x, y) => ... action to perform when dragging ends
+function createDragAction(registerDisplayObject, dragTarget, dragStartAction, dragMoveAction, dragEndAction) {
   let dragging = false
-  let dragStart = null
+  let previousMousePoint = null
+
+  if (!dragStartAction)
+    dragStartAction = (x, y) => {} // default dragstart action is NOP
 
   // default action is to translate the target object's position point
-  if (!displaceFunc)
-    displaceFunc = (dx, dy) => {dragTarget.x += dx; dragTarget.y += dy}
+  if (!dragMoveAction)
+    dragMoveAction = (dx, dy) => {dragTarget.x += dx; dragTarget.y += dy}
+
+  if (!dragEndAction)
+    dragEndAction = (x, y) => {} // default ending action is NOP
   
   // while dragging, apply displaceFunc to the change in mouse position
   let onDragMove = function(mouseEvent) {
     if (dragging) {
-      const { x, y } = dragStart
-      dragTarget.parent.toLocal(mouseEvent.global, null, dragStart)
-      displaceFunc(dragStart.x - x, dragStart.y - y)
+      const { x, y } = previousMousePoint
+      dragTarget.parent.toLocal(mouseEvent.global, null, previousMousePoint)
+      dragMoveAction(previousMousePoint.x - x, previousMousePoint.y - y)
     }
   }
 
@@ -128,15 +138,18 @@ function mkDraggable(registerDisplayObject, dragTarget, displaceFunc) {
   let onDragStart = function(mouseEvent) {
     dragging = true
     app.stage.on('pointermove', onDragMove)
-    dragStart = dragTarget.parent.toLocal(mouseEvent.global)
+    previousMousePoint = dragTarget.parent.toLocal(mouseEvent.global)
+    dragStartAction(previousMousePoint.x, previousMousePoint.y)
   }
 
   // deregister the callback placed on the stage to stop dragging
-  let onDragEnd = function() {
+  let onDragEnd = function(mouseEvent) {
     if (dragging) {
       app.stage.off('pointermove', onDragMove)
       dragging = false
-      dragStart = null
+      previousMousePoint = null
+      const {x,y} = dragTarget.parent.toLocal(mouseEvent.global)
+      dragEndAction(x,y)
     }
   }
 
