148343af62cd18d7ccb0ee27e796f7ce5af19ca2
= <mbennett3@email.arizona.edu>
2023-03-17 13:47:32 -0700

    basic sprite gen kind of works but needs tweaked

--- spritegen.js
+++ spritegen.js
@@ -7,8 +7,8 @@
 class SpriteGenerator {
 
     //Amplitude constraints
-    static maxAmplitude = 5;
-    static minAmplitude = 0.5;
+    static maxAmplitude = 200;
+    static minAmplitude = 100;
     //Wavelength constraints
     static maxFrequency = 2;
     static minFrequency = 0.5;
@@ -18,7 +18,10 @@ class SpriteGenerator {
 
     //Spacing between body parts
     //Might make this a range too idk
-    static bodyPartSpacing = 20;
+    static bodyPartSpacing = 80;
+
+    static baseBodyPartHeight = 128;
+    static baseBodyPartWidth = 128;
 
     //internal
     //return a random decimal between min and max
@@ -39,17 +42,22 @@ class SpriteGenerator {
         var n_parts = Math.floor(this.rand_dec(this.minBodyParts, this.maxBodyParts));
         var x_head = 0; //x point to start generating at; might need to tweak
 
+        console.log(amplitude, frequency, n_parts, x_head);
         //generate body part postions
         var sample_x = Array(n_parts);
         var sample_y = Array(sample_x.length);
         var xpos = x_head; 
+
+        //Choose samples from the sine curve
+        //TODO: Samples shouldn't be separated by the fixed x
+        //but instead should be equidistant wrt arc length
         for(var i=0;i<sample_y.length;i++) {
             sample_x[i] = xpos;
-            sample_y[i] = i;
-            //sample_y[i] = amplitude * Math.cos( frequency * sample_x );
+            //sample_y[i] = i;
+            sample_y[i] = amplitude * Math.cos( frequency * sample_x[i] );
             xpos += this.bodyPartSpacing;
         }
-
+        console.log(sample_x, sample_y);
         var spriteCont = new PIXI.Container;
 
         for(var i=0; i<sample_x.length;i++) {
@@ -60,7 +68,25 @@ class SpriteGenerator {
             //TODO: will probably need to scale the sprites
             chunk.x = sample_x[i];
             chunk.y = sample_y[i];
-            //TODO: rotate it to point towards the part in front of it
+            chunk.width = this.baseBodyPartWidth;
+            chunk.height = this.baseBodyPartHeight;
+            //Get rotation
+            if(i==0) {
+                //how to rotate the head?
+                console.log("head");
+            }
+            else{
+                //align the body part to point
+                //to the part in front of it
+                var front_x = sample_x[i-1];
+                var front_y = sample_y[i-1];
+                var dx = sample_x[i] - sample_x[i-1];
+                var dy = sample_y[i] - sample_y[i-1];
+                var rot = Math.atan(dy/dx);
+                chunk.rotation = rot;
+
+
+            }
             
             //set anchor and add to container
             chunk.anchor.set(0.5);
