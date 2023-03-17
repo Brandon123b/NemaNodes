cbdd9121c1c7d1098f9efec0b6bda446248be89b
= <mbennett3@email.arizona.edu>
2023-03-17 14:45:09 -0700

    approximate arc length to place chunks according to spacing, playing with params

--- spritegen.js
+++ spritegen.js
@@ -18,7 +18,10 @@ class SpriteGenerator {
 
     //Spacing between body parts
     //Might make this a range too idk
-    static bodyPartSpacing = 80;
+    //could be interpreted as 1/sampling rate
+    //sampling rate needs to be double freq
+    //(shannon-nyquist theorem)
+    static bodyPartSpacing = 20;
 
     static baseBodyPartHeight = 128;
     static baseBodyPartWidth = 128;
@@ -37,27 +40,54 @@ class SpriteGenerator {
         await PIXI.Assets.load('Bibite.png');
 
         //create random parameters
-        var amplitude = this.rand_dec(this.minAmplitude, this.maxAmplitude);
-        var frequency = this.rand_dec(this.minFrequency, this.maxFrequency);
-        var n_parts = Math.floor(this.rand_dec(this.minBodyParts, this.maxBodyParts));
+        //var amplitude = this.rand_dec(this.minAmplitude, this.maxAmplitude);
+        //var frequency = this.rand_dec(this.minFrequency, this.maxFrequency);
+        //var n_parts = Math.floor(this.rand_dec(this.minBodyParts, this.maxBodyParts));
         var x_head = 0; //x point to start generating at; might need to tweak
+
+        //TESTING PARAMS
+        var amplitude = 50;
+        var frequency = 0.05;
+        var n_parts = 10;
 
         console.log(amplitude, frequency, n_parts, x_head);
         //generate body part postions
         var sample_x = Array(n_parts);
         var sample_y = Array(sample_x.length);
-        var xpos = x_head; 
+        
 
         //Choose samples from the sine curve
         //TODO: Samples shouldn't be separated by the fixed x
         //but instead should be equidistant wrt arc length
-        for(var i=0;i<sample_y.length;i++) {
-            sample_x[i] = xpos;
+
+        var chunks_placed = 0;
+        var stepsize = 1;
+        var xnew, ynew; 
+        var xold = x_head;
+        var yold = amplitude * Math.cos( frequency * xold);
+        var s=0; //distance travelled along arc since last chunk place
+        var dx, dy, ds
+        for(xnew=x_head;chunks_placed < n_parts;xnew+= stepsize) {
+            ynew = amplitude * Math.cos(frequency * xnew);
+            dx = xnew - xold;
+            dy = ynew - yold;
+            ds = Math.sqrt((dx+dy)**2);
+            s += ds;
+            if(s>=this.bodyPartSpacing) {
+                sample_x[chunks_placed] = xnew;
+                sample_y[chunks_placed] = ynew;
+                s=0;
+                chunks_placed++;
+            }
+            xold = xnew;
+            yold = ynew;
+            
+            //sample_x[i] = xpos;
             //sample_y[i] = i;
-            sample_y[i] = amplitude * Math.cos( frequency * sample_x[i] );
-            xpos += this.bodyPartSpacing;
+            //sample_y[i] = amplitude * Math.cos( frequency * sample_x[i] );
+            
         }
-        console.log(sample_x, sample_y);
+        console.log("samples: " ,sample_x, sample_y);
         var spriteCont = new PIXI.Container;
 
         for(var i=0; i<sample_x.length;i++) {
